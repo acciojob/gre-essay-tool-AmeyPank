f2c89423d1e24d4d51caa4723d28a14412ac123f
Amey  Pankanti <86099941+AmeyPank@users.noreply.github.com>
2023-04-14 01:58:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your code here
+function countChar(evaluatedTExt,wordCount){
+	var length = document.getElementById(evaluatedTExt).value.length;
+	document.getElementById(wordCount).innerHTML = length;
+}
