924e0da7216fe8db44985bef18e17e1c8695214c
AmeyPank <pankantiameya@.com>
2023-04-14 02:08:17 +0530

    Formated

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //your code here
- function fun() {
-              
-                var str = document
-                    .getElementById("evaluatedText").value;
-              
-                // Initialize the word counter
-                var count = 0;
-              
-             
-                var splited = str.split(' ');
-              
-               
-                for (var i = 0; i < splited.length; i++) {
-                    if (splited[i] != "") {
-                        count += 1;
-                    }
-                }
-              
-              
-                document.getElementById("wordCount")
-                    .innerHTML = count;
-            }
+function fun() {
+
+    var str = document
+        .getElementById("evaluatedText").value;
+
+    // Initialize the word counter
+    var count = 0;
+
+
+    var splited = str.split(' ');
+
+
+    for (var i = 0; i < splited.length; i++) {
+        if (splited[i] != "") {
+            count += 1;
+        }
+    }
+
+
+    document.getElementById("wordCount")
+        .innerHTML = count;
+}
